27d3e23b219499e77b57378673bc9b48968540aa
nancymi <13121178nancy@gmail.com>
2016-03-01 18:15:34 +0800

    Finish: add source about dictionary.

--- main/source.js
+++ main/source.js
@@ -1,3 +1,21 @@
 /**
  * Created by nancymi on 16/3/1.
  */
+
+var engDigits = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
+
+var engTens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+
+var metrics = [ '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion' ];
+
+var ZERO = 'zero';
+
+function loadDictionary() {
+    return {
+        engTens: engTens,
+        engDigits: engDigits,
+        metrics: metrics,
+        ZERO: ZERO
+    }
+}
