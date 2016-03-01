bf67ffc60d52c9642e23c62bbf2b6f6139164cb4
nancymi <13121178nancy@gmail.com>
2016-03-01 18:16:53 +0800

    Finish: init structure of main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
 /**
  * Created by nancymi on 16/2/29.
  */
+
+function translateNumberInEnglish(number) {
+    var numberMetricList = divideIntoMetric(number);
+    var numberStrList = doTranslate(numberMetricList);
+    var numberStr = getNumberStr(numberStrList);
+
+    return numberStr;
+}
+
